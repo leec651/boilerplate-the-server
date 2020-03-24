2ffd3f020465659f68d614f13631a944d4f8f589
Chen Wei Lee <leec651@gmail.com>
2020-03-24 00:17:32 -0400

    convert everything to coffee

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div#app
     div#nav
-      router-link(to="/") Home |
+      router-link(to="/") Home |&nbsp;
       router-link(to="/about") About
     router-view
 </template>
